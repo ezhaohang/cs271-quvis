22a184ec31ffcd4b039f424c98b6b8cf17b43fd5
QuVis <ezhaohang@gmail.com>
2020-03-24 18:02:18 +0000

    Python Anywhere test

--- qusite/settings.py
+++ qusite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_mx=_iv*qimd8!0d6xhdpwg5wr7nyyth*gx$v(v!rb#ta!5_w-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quvis.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
